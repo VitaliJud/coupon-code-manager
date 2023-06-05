3da1b6d1ec3f5035b2880deb55af782b0b32b8ad
Vitali Judin <65638870+VitaliJud@users.noreply.github.com>
2023-06-05 17:34:04 -0500

    Update [promotionId].tsx
    Updating Checkboxes

--- pages/promotions/[promotionId].tsx
+++ pages/promotions/[promotionId].tsx
@@ -11,100 +11,156 @@ import Loading from '../../components/loading'
 import { useCodes } from '../../lib/hooks'
 
 const Promotion = () => {
-    const router = useRouter()
-    const promotionId = Number(router.query?.promotionId)
-    const [itemsPerPage, setItemsPerPage] = useState(10)
-    const [currentPage, setCurrentPage] = useState(1)
-    const [generating, setGenerating] = useState(false)
-    const [exporting, setExporting] = useState(false)
-    const { error, isLoading, list = [], meta = {}, mutateList } = useCodes(promotionId, {
-        page: String(currentPage),
-        limit: String(itemsPerPage),
+  const router = useRouter()
+  const promotionId = Number(router.query?.promotionId)
+  const [itemsPerPage, setItemsPerPage] = useState(10)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [generating, setGenerating] = useState(false)
+  const [exporting, setExporting] = useState(false)
+  const [selectedCodes, setSelectedCodes] = useState([])
+  const { error, isLoading, list = [], meta = {}, mutateList } = useCodes(promotionId, {
+    page: String(currentPage),
+    limit: String(itemsPerPage),
+  })
+  const itemsPerPageOptions = [10, 20, 50, 100, 250]
+  const tableItems: CouponListItem[] = list.map(({ id, code, max_uses, max_uses_per_customer, current_uses, created }) => ({
+    id,
+    code,
+    max_uses,
+    max_uses_per_customer,
+    current_uses,
+    created,
+  }))
+
+  const onItemsPerPageChange = newRange => {
+    setCurrentPage(1)
+    setItemsPerPage(newRange)
+  }
+
+  const renderCode = (code: string): ReactElement => <Text>{code}</Text>
+
+  const renderCurrentUses = (uses: number): ReactElement => <Small>{uses}</Small>
+
+  const renderMaxUses = (uses: number): ReactElement => <Small>{uses ? uses : String.fromCharCode(0x221E)}</Small>
+
+  const renderDate = (date: string): ReactElement => <Text>{date}</Text> // TODO: Convert to datestring
+
+  const handleCodeCheckboxChange = codeId => {
+    setSelectedCodes(prevSelectedCodes => {
+      if (prevSelectedCodes.includes(codeId)) {
+        return prevSelectedCodes.filter(id => id !== codeId)
+      } else {
+        return [...prevSelectedCodes, codeId]
+      }
     })
-    const itemsPerPageOptions = [10, 20, 50, 100, 250]
-    const tableItems: CouponListItem[] = list.map(({ id, code, max_uses, max_uses_per_customer, current_uses, created}) => ({
-        id,
-        code,
-        max_uses,
-        max_uses_per_customer,
-        current_uses,
-        created,
-    }))
+  }
 
-    const onItemsPerPageChange = newRange => {
-        setCurrentPage(1)
-        setItemsPerPage(newRange)
+  const handleAllCodesCheckboxChange = () => {
+    if (selectedCodes.length === tableItems.length) {
+      setSelectedCodes([])
+    } else {
+      setSelectedCodes(tableItems.map(item => item.id))
     }
+  }
 
-    const renderCode = (code: string): ReactElement => (
-        <Text>{code}</Text>
-    )
+  const handleDeleteSelected = () => {
+    if (window.confirm('Are you sure you want to delete the selected codes?')) {
+      const deletionPromises = selectedCodes.map(codeId => {
+        return fetch(`/api/promotions/${promotionId}/codes?id:in=${codeId}`, { method: 'DELETE' })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(`Error deleting code with ID ${codeId}`)
+            }
+          })
+      })
 
-    const renderCurrentUses = (uses: number): ReactElement => (
-        <Small>{uses}</Small>
-    )
+      Promise.all(deletionPromises)
+        .then(() => {
+          setSelectedCodes([])
+          // Show a success message
+          alert('Selected codes deleted successfully!')
+        })
+        .catch(error => {
+          // Handle any errors
+          console.error('Error deleting codes:', error)
+          // Show an error message
+          alert('Error deleting codes. Please try again.')
+        })
+    }
+  }
 
-    const renderMaxUses = (uses: number): ReactElement => (
-        <Small>{uses ? uses : String.fromCharCode(0x221E)}</Small>
-    )
+  if (isLoading) return <Loading />
+  if (error) return <ErrorMessage error={error} />
 
-    const renderDate = (date: string): ReactElement => (
-        <Text>{date}</Text> // TODO: Convert to datestring
-    )
-
-    if (isLoading) return <Loading />
-    if (error) return <ErrorMessage error={error} />
-
-    return (
-        <Panel header="Coupon Codes">
-            {generating 
-            && <CodeGeneratorModal
-                promotionId={promotionId}
-                onClose={() => { 
-                    setGenerating(false)
-                    mutateList()
-                }}
-            />}
-            {exporting
-            && <ExportCodesModal 
-                promotionId={promotionId}
-                onClose={() => setExporting(false)}
-            />}
-            <Flex justifyContent={'space-between'}>
-                <Box>
-                    <Link href="/">
-                        <StyledLink>{'<- Back to Promotions'}</StyledLink>
-                    </Link>
-                </Box>
-                <Box>
-                    <Button iconLeft={<ArrowDownwardIcon />} onClick={() => setExporting(true)}>Export Coupons</Button>
-                    <Button iconLeft={<AddIcon />} onClick={() => setGenerating(true)}>Generate Coupons</Button>
-                </Box>
-            </Flex>
-            <Table 
-                columns={[
-                    { header: 'Coupon Code', hash: 'code', render: ({code}) => renderCode(code)},
-                    { header: 'Created', hash: 'created', render: ({created}) => renderDate(created)},
-                    { header: 'Current Uses', hash: 'current_uses', render: ({current_uses}) => renderCurrentUses(current_uses)},
-                    { header: 'Max Uses', hash: 'max_uses', render: ({max_uses}) => renderMaxUses(max_uses)},
-                    { header: 'Max Uses Per Customer', hash: 'max_uses_per_customer', render: ({max_uses_per_customer}) => renderMaxUses(max_uses_per_customer)},
-                ]}
-                items={tableItems}
-                itemName="Coupon Codes"
-                pagination={{
-                    currentPage,
-                    totalItems: meta.pagination?.total,
-                    onPageChange: setCurrentPage,
-                    itemsPerPageOptions,
-                    onItemsPerPageChange,
-                    itemsPerPage,
-                }}
-                stickyHeader
-            >
-
-            </Table>
-        </Panel>
-    )
+  return (
+    <Panel header="Coupon Codes">
+      {generating && (
+        <CodeGeneratorModal
+          promotionId={promotionId}
+          onClose={() => {
+            setGenerating(false)
+            mutateList()
+          }}
+        />
+      )}
+      {exporting && (
+        <ExportCodesModal promotionId={promotionId} onClose={() => setExporting(false)} />
+      )}
+      <Flex justifyContent="space-between">
+        <Box>
+          <Link href="/">
+            <StyledLink>{'<- Back to Promotions'}</StyledLink>
+          </Link>
+        </Box>
+        <Box>
+          <Button iconLeft={<ArrowDownwardIcon />} onClick={() => setExporting(true)}>
+            Export Coupons
+          </Button>
+          <Button iconLeft={<AddIcon />} onClick={() => setGenerating(true)}>
+            Generate Coupons
+          </Button>
+          <Button onClick={handleDeleteSelected}>Delete Selected</Button>
+        </Box>
+      </Flex>
+      <Table
+        columns={[
+          {
+            header: (
+              <input
+                type="checkbox"
+                checked={selectedCodes.length === tableItems.length}
+                onChange={handleAllCodesCheckboxChange}
+              />
+            ),
+            hash: 'checkbox',
+            render: ({ id }) => (
+              <input
+                type="checkbox"
+                checked={selectedCodes.includes(id)}
+                onChange={() => handleCodeCheckboxChange(id)}
+              />
+            ),
+          },
+          { header: 'Coupon Code', hash: 'code', render: ({ code }) => renderCode(code) },
+          { header: 'Created', hash: 'created', render: ({ created }) => renderDate(created) },
+          { header: 'Current Uses', hash: 'current_uses', render: ({ current_uses }) => renderCurrentUses(current_uses) },
+          { header: 'Max Uses', hash: 'max_uses', render: ({ max_uses }) => renderMaxUses(max_uses) },
+          { header: 'Max Uses Per Customer', hash: 'max_uses_per_customer', render: ({ max_uses_per_customer }) => renderMaxUses(max_uses_per_customer) },
+        ]}
+        items={tableItems}
+        itemName="Coupon Codes"
+        pagination={{
+          currentPage,
+          totalItems: meta.pagination?.total,
+          onPageChange: setCurrentPage,
+          itemsPerPageOptions,
+          onItemsPerPageChange,
+          itemsPerPage,
+        }}
+        stickyHeader
+      ></Table>
+    </Panel>
+  )
 }
 
 export default Promotion
