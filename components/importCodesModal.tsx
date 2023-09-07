286df553ec9728430fd79f2cbc452c90b5ae94a4
Vitali Judin <65638870+VitaliJud@users.noreply.github.com>
2023-09-07 00:51:41 -0500

    Update importCodesModal.tsx

--- components/importCodesModal.tsx
+++ components/importCodesModal.tsx
@@ -1,73 +1,86 @@
+import { Box, Button, Message, Modal, ModalAction, Stepper, Text, } from '@bigcommerce/big-design';
 import React, { useState } from 'react';
-import { Box, Button, Modal, ModalAction, ProgressBar, Stepper, Text, Message, Link, } from '@bigcommerce/big-design';
-import { useSession } from '../context/session';
+// import { useSession } from '../context/session';    //Enable when encodedContext is required
 
 interface ImportCodesModalProps {
-    promotionId: number,
+    // promotionId: number,
     onClose: () => void
 }
 
-const ImportCodesModal = ({ promotionId, onClose }: ImportCodesModalProps) => {
-  const encodedContext = useSession()?.context;
-  const [currentStep, setCurrentStep] = useState(0);
-  const [uploading, setUploading] = useState(false);
-  const [abortController, setAbortController] = useState(null)
-  const [file, setFile] = useState(null);
-  const [error, setError] = useState(null);
-
-  const handleFileChange = (event) => {
+const ImportCodesModal = ({ onClose }: ImportCodesModalProps) => {
+    // const encodedContext = useSession()?.context;    // Will need to be used when parsing is happening and codes need to be created
+    const [currentStep, setCurrentStep] = useState(0);
+    // const [uploading, setUploading] = useState(false);    // Need to include CSV Parse such as PapaParse
+    const [abortController, setAbortController] = useState(null)
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+    
+    const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
     setFile(selectedFile);
-  };
-
-  const handleUpload = async () => {
-    if (!file) {
-      setError('Please select a CSV file to upload.');
-      return;
-    }
-
+    };
+    
+    // const handleUpload = async () => {
+    // if (!file) {
+    //   setError('Please select a CSV file to upload.');
+    //   return;
+    // }
+    
     // Implement your CSV parsing and code creation logic here
-
+    
     // For example, you can use the `PapaParse` library to parse the CSV file
     // and create codes based on the headers.
-
+    
     // After processing, you can update the `currentStep` accordingly.
-
+    
     setCurrentStep(1); // Set to the next step after processing
-
+    
     // You can also use the `postCoupon` function to create codes as needed.
-  };
-
-  const handleClose = () => {
+    };
+    
+    if(currentStep === 0 && !abortController) {
+        try {
+            const ac = new AbortController()
+            setAbortController(ac)
+            fetchCoupons(ac.signal)
+        } catch (error) {
+            if (error.message != "The user aborted a request." ) {
+                console.error(error)
+                setCurrentStep(1)
+            }
+        }
+    }
+    
+    const handleClose = () => {
       abortController.abort()
       onClose()
-  }
-
-  const handleStop = () => {
+    }
+    
+    const handleStop = () => {
       abortController.abort()
       setCurrentStep(1)
-  }
-  
-  const renderActions = (): ModalAction[] => {
+    }
+    
+    const renderActions = (): ModalAction[] => {
     if (currentStep === 0) {
       return [
                 {text: 'Stop Import and Close', variant: 'subtle', onClick: handleClose},
                 {text: 'Stop Import and Download Codes', variant: 'primary', onClick: handleStop}
             ]
         }
-
+    
         if (currentStep == 1) {
             return [
                 { text: 'Close', variant: 'subtle', onClick: handleClose}
             ]
         }
     }
-
+    
     const renderOnModalClose = () => {
         if (currentStep == 0) {
             return handleClose
         }
-
+    
         if (currentStep == 1) {
             return onClose
         }
@@ -151,7 +164,7 @@ const ImportCodesModal = ({ promotionId, onClose }: ImportCodesModalProps) => {
       isOpen={true}
       actions={renderActions()}
       header="Import Coupons"
-      onClose={onClose}
+      onClose={renderOnModalClose()}
       closeOnClickOutside={false}
       closeOnEscKey={false}
     >
